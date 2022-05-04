e478594777dda6ef78e2c1212512fa10f32f3259
lukmannudin <lukmannudin.priatna@happyfresh.com>
2022-05-04 23:09:26 +0700

    refactor 3sum

--- src/leetcode/3Sum.kt
+++ src/leetcode/3Sum.kt
@@ -3,54 +3,72 @@ package leetcode
 object `3Sum` {
     @JvmStatic
     fun main(args: Array<String>) {
-        val result = threeSum(intArrayOf(0,0,0))
-        for (r in result) {
-            println(r)
+//        [-1,0,1,2,-1,-4]
+//        val sums = threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
+        val sums = threeSum(intArrayOf(0,0,0,0))
+        for (sum in sums) {
+            print("$sum,")
         }
     }
 
-    @Suppress("KotlinConstantConditions")
     fun threeSum(nums: IntArray): List<List<Int>> {
-        val result = mutableListOf<List<Int>>()
+        val results = mutableListOf<List<Int>>()
+        var outerPointer = 0
+        var lastOuterPointer: Int? = null
+        val sortedNums = nums.sortedArray()
 
-        // sorted because to make easier identify duplicated value
-        val sortedNums = nums.sorted()
-
-        for (i in sortedNums.indices) {
-            if (i > 0 && sortedNums[i] == sortedNums[i - 1]) {
+        while (outerPointer < sortedNums.size) {
+            if (isPass(sortedNums, lastOuterPointer, outerPointer)) {
+                outerPointer++
                 continue
             }
 
-            var j = i + 1
-            var k = nums.size - 1
+            lastOuterPointer = outerPointer
+            var innerPointerA = outerPointer + 1
+            var innerPointerB = sortedNums.size - 1
 
-            if (i == j || j == k || k == i) {
-                continue
-            }
+            var lastInnerPointerA: Int? = null
+            var lastInnerPointerB: Int? = null
+            while (innerPointerA < innerPointerB) {
 
-            while (j < k) {
-                val currentSums = listOf(sortedNums[i], sortedNums[j], sortedNums[k])
-                val resultNum = currentSums.sum()
-                if (resultNum >= 0) {
-                    if (resultNum == 0) {
-                        result.add(currentSums)
+                if (isPass(sortedNums, lastInnerPointerA, innerPointerA)) {
+                    innerPointerA++
+                    continue
+                }
+
+                if (isPass(sortedNums, lastInnerPointerB, innerPointerB)) {
+                    innerPointerB--
+                    continue
+                }
+
+                lastInnerPointerA = innerPointerA
+                lastInnerPointerB = innerPointerB
+
+//                println("${sortedNums[outerPointer]},${sortedNums[innerPointerA]},${sortedNums[innerPointerB]}")
+                val sum = sortedNums[outerPointer] + sortedNums[innerPointerA] + sortedNums[innerPointerB]
+                if (sum >= 0) {
+                    if (sum == 0) {
+                        results.add(listOf(sortedNums[outerPointer], sortedNums[innerPointerA], sortedNums[innerPointerB]))
                     }
-                    k--
-                    while (
-                        (k + 1 < sortedNums.size && k >= 0) && sortedNums[k + 1] == sortedNums[k]
-                    ) {
-                        k--
-                    }
-                } else if (resultNum < 0) {
-                    j++
-                    while (
-                        (j - 1 >= 0 && j < sortedNums.size) && sortedNums[j - 1] == sortedNums[j]
-                    ) {
-                        j++
-                    }
+                    innerPointerB--
+                } else {
+                    innerPointerA++
                 }
             }
+
+            outerPointer++
         }
-        return result
+
+        return results
+    }
+
+    fun isPass(nums: IntArray, lastPointer: Int?, pointer: Int): Boolean {
+        return if (lastPointer == null || lastPointer == pointer) {
+            false
+        } else {
+            val lastPointerValue = nums[lastPointer]
+            val currentPointerValue = nums[pointer]
+            lastPointerValue == currentPointerValue
+        }
     }
 }
